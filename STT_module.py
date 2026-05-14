1ede67336886229efd5f06ece4d2aaf9b48ea76b
Linlin Li <lilinlin0502@outlook.com>
2026-05-14 21:26:50 +0200

    update

--- STT_module.py
+++ STT_module.py
@@ -2,7 +2,7 @@ import os
 from faster_whisper import  WhisperModel
 
 class SpeechToText:
-    def __init__(self, model_size="small", device="cuda", compute_type="float16"):
+    def __init__(self, model_size="tiny", device="cpu", compute_type="int8"):
         self.model = WhisperModel(model_size, device=device, compute_type=compute_type)
 
     def transcribe(self, audio_path):
@@ -11,7 +11,7 @@ class SpeechToText:
             beam_size=5,
             vad_filter=True,
             language="en",
-            initial_prompt="This is a conversation about an art school called HBKsaar in Germany.")
+            initial_prompt="This is a conversation about an art university called HBKsaar in Germany.")
 
         transcription = ""
         for segment in segments:
